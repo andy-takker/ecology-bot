2a7865bbdb36cf5f73fc2f08190e4848741d0338
Sergey Natalenko <sergey.natalenko@mail.ru>
2022-10-14 16:03:01 +0300

    Update:

--- ecology_bot/bot/main.py
+++ ecology_bot/bot/main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from aiocache import Cache
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.types import ParseMode
